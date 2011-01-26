b5597f545f4703fd504fd581a6f70656e499a08d
jeff <leterip@gmail.com>
2011-01-26 07:33:57 -0500

    added admin to installed apps

--- settings.py
+++ settings.py
@@ -82,9 +82,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.admin',
     'south',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
