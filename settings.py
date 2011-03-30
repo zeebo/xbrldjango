8653d6421344ce24828f50498aed66016d92fd28
jeff <leterip@gmail.com>
2011-03-30 14:20:34 -0400

    added profiling to djdt

--- settings.py
+++ settings.py
@@ -40,6 +40,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -66,6 +67,23 @@ INSTALLED_APPS = (
     'south',
     'djangosphinx',
     'compilation',
+    'debug_toolbar',
 )
 
+#For compilation
 COMPILER_ROOT = os.path.join(CURRENT_PATH, 'static/comp')
+
+#For django debug toolbar
+INTERNAL_IPS = ('127.0.0.1',)
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+    'djdt_profile.djdt_profile.ProfilingPanel',
+)
